f044cc2538090790589bf61782561dbed0fc862e
cocktailpeanut <cocktailpeanuts@proton.me>
2024-10-07 09:29:03 -0400

    init

---
+++ install.js
@@ -0,0 +1,75 @@
+module.exports = {
+  run: [
+    // Edit this step to customize the git repository to use
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://github.com/peanutcocktail/FacePoke app",
+        ]
+      }
+    },
+    // Edit this step with your custom install commands
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "pip install -r requirements_base.txt"
+        ]
+      }
+    },
+    {
+      when: "{{platform === 'darwin'}}",
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "pip install onnxruntime-silicon"
+        ]
+      }
+    },
+    {
+      when: "{{gpu === 'nvidia'}}",
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "pip install onnxruntime-gpu"
+        ]
+      }
+    },
+    {
+      when: "{{gpu !== 'nvidia' && platform !== 'darwin'}}",
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "pip install onnxruntime"
+        ]
+      }
+    },
+    // Delete this step if your project does not use torch
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "app",                // Edit this to customize the path to start the shell from
+          // xformers: true   // uncomment this line if your project requires xformers
+        }
+      }
+    },
+    {
+      method: "fs.link",
+      params: {
+        venv: "app/env"
+      }
+    }
+  ]
+}
