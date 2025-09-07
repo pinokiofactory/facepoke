2cfc92ba2637a97bdf61e8caa0e615aa38565d18
6Morpheus6 <wanton-decay@gmx.net>
2025-09-07 05:22:03 +0200

    move onnxruntime to torch.js, use uv

--- install.js
+++ install.js
@@ -16,40 +16,7 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install -r requirements_base.txt"
-        ]
-      }
-    },
-    {
-      when: "{{platform === 'darwin'}}",
-      method: "shell.run",
-      params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
-        message: [
-          "pip install onnxruntime-silicon"
-        ]
-      }
-    },
-    {
-      when: "{{gpu === 'nvidia'}}",
-      method: "shell.run",
-      params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
-        message: [
-          "pip install onnxruntime-gpu"
-        ]
-      }
-    },
-    {
-      when: "{{gpu !== 'nvidia' && platform !== 'darwin'}}",
-      method: "shell.run",
-      params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
-        message: [
-          "pip install onnxruntime"
+          "uv pip install -r requirements_base.txt"
         ]
       }
     },
@@ -64,12 +31,6 @@ module.exports = {
           // xformers: true   // uncomment this line if your project requires xformers
         }
       }
-    },
-//    {
-//      method: "fs.link",
-//      params: {
-//        venv: "app/env"
-//      }
-//    }
+    }
   ]
 }
