332ed3ddc56c6971328f63c88e19cf5e09e00923
6Morpheus6 <wanton-decay@gmx.net>
2025-09-07 05:35:53 +0200

    install wheel, no build isolation

--- install.js
+++ install.js
@@ -16,7 +16,8 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "uv pip install -r requirements_base.txt"
+          "uv pip install wheel",
+          "uv pip install -r requirements_base.txt --no-build-isolation"
         ]
       }
     },
